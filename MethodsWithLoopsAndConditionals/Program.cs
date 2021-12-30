bdaae01fcee0f72078796ccea91985e72e7eb4bc
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-30 14:02:04 -0600

    Add project files.

---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintThousand();
+            Threes();
+        }
+        public static void PrintThousand()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Threes()
+        {
+
+            for (int x =3; x <=999; x += 3)
+            {
+                Console.WriteLine(x);
+            }
+        } 
+        
+        
+            
+            
+            
+            
+
+        
+
+
+
+    }
+}       
